48e941ddd6b9ded032af0d6638a9c184d00e9c2b
Tyler Dailey <tkdailey11@gmail.com>
2022-10-15 22:30:55 -0600

    Initial Commit

---
+++ template/src/App.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from "react"
+import {BrowserRouter} from "react-router-dom"
+
+function App(): ReactElement {
+  return (
+    <BrowserRouter>
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  )
+}
+
+export default App
